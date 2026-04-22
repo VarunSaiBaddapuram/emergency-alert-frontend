c6e7de8d24613ec58db7d305893122a205d0b729
Varun Sai Baddapuram <weirdovarun@gmail.com>
2026-04-22 19:43:50 +0530

    updated with structure

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,15 @@ import { theme } from "./theme";
 import { CssBaseline } from "@mui/material";
 import "bootstrap/dist/css/bootstrap.css";
 import "../src/scenes/css/index.css";
+
+// Suppress the ReactAnimatedWeather defaultProps warning
+const originalError = console.error;
+console.error = (...args) => {
+  if (args[0] && typeof args[0] === 'string' && args[0].includes('Support for defaultProps will be removed')) {
+    return;
+  }
+  originalError(...args);
+};
  
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
